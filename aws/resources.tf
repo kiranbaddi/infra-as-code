033ad5c8607a4298a75409c6257512c0cbc7fc1d
kiranbaddi <kiran.baddi@hotmail.com>
2025-06-19 08:12:52 +0200

    resource to provision VPC and subnets with the module

---
+++ aws/resources.tf
@@ -0,0 +1,18 @@
+module "learnops_vpc" {
+  source                 = "./modules/vpc"
+  PREFIX                 = "learnops"
+  REGION                 = "eu-central-1"
+  ENVIRONMENT            = "development"
+  VPC_CIDR_BLOCK         = "10.0.0.0/16"
+  NAT_GATEWAY_CIDR_BLOCK = "10.0.0.0/28"
+  subnets = {
+    "web" = {
+      subnet_cidr = "10.0.10.0/24",
+      public      = true
+    },
+    "app" = {
+      subnet_cidr = "10.0.15.0/24",
+      public      = true
+    }
+  }
+}
