8a7668a3929d024f3388b85ca7396bd752f43d9e
kiranbaddi <kiran.baddi@hotmail.com>
2025-06-25 19:34:06 +0200

    created new bucket in eu-central-1

--- aws/provider.tf
+++ aws/provider.tf
@@ -7,7 +7,7 @@ terraform {
 
   }
   backend "s3" {
-    bucket         = "terraform-state-30564"
+    bucket         = "tf-state-29819"
     key            = "global/s3/terraform.tfstate"
     region         = "eu-central-1"
     dynamodb_table = "terraform-lock-db"
